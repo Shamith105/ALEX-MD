19d3bfa9bca2440231ae302d92ef0ce748fcd2cd
Arajuna <155340873+Arajuna@users.noreply.github.com>
2024-08-21 13:03:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,23 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-ALIVE_MSG: process.env.ALIVE_MSG,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/d7e4a702a1b70a78c29ba.jpg",
-
+ALIVE_MSG: process.env.ALIVE_MSG || "*╭─ `「  𝐀𝙻𝗘𝙓-𝗠𝗗  」`*
+*╰────────────┈*
+> ⟡━⟪ 𝙸 𝙰𝙼 𝙰𝙻𝙸𝚅𝙴  ⟫━⟡
+*╭────────────┈◦•◦❥•*
+*╎➮ .𝙲𝚁𝙴𝙰𝚃𝙴𝙳 𝙱𝚈 𝙼𝚁 𝙰𝙻𝙴𝚇-𝙸𝙳*
+*╎➮ .𝙼𝙾𝚅𝙸𝙴 𝚂𝙴𝚁𝙲𝙷 𝚂𝚈𝚂𝚃𝙴𝙼*
+*╎➮ .𝙱𝚄𝙶𝚂*
+*╎➮ .𝙿𝙰𝙸𝙳 𝙵𝙸𝙻𝙴*
+*╚──────────────⬣>*
+*╭─ `「  𝙐𝙋𝘿𝘼𝙏𝙀𝙎  」`*
+*╰───────────────┈*
+            *⟪  Follow  Us  ⟫*
+https://whatsapp.com/channel/0029VagEOTtHwXbDdrr7Eq3k          
+     
+          `𝙋𝙊𝙒𝙀𝙍𝙀𝘿 𝘽𝙔`
+> ⟡━⟪  𝘔𝘙 𝘈𝘓𝘌𝘟 - 𝘐𝘋  ⟫━⟡
+           `⦁│ᴘʀᴏɢʀᴀᴍꜱ│⦁`",
+    
 };
