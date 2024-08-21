7207454d1236024c6878e6213e9eb81219fbf22b
CYBER-MD <167730742+CYBER-MD@users.noreply.github.com>
2024-08-21 17:25:59 +0530

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -1,15 +1,19 @@
 const config = require('../config')
-const {cmd , commands} = require('../command')
-
+const { cmd , commands } = require('../command')
+const {sleep} = require('../lib/functions')
 cmd({
-    pattern: "alive",
-    desc: "Check bot online or no.",
-    category: "main",
+    pattern: "restart", 
+    desc: "CYBER-MD restart bot..✅",
+    category: "owner",  
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
+if (!isOwner) return reply("*This Owner Command*");
+const {exec} = require("child_process")
+reply("CYBER-MD restarting..👨‍💻✅")
+await sleep(1500)
+exec("pm2 restart all")
 }catch(e){
 console.log(e)
 reply(`${e}`)
