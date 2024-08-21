4f89f27376613a6ad9a52ea7664fb97616bc09ec
CYBER-MD <167730742+CYBER-MD@users.noreply.github.com>
2024-08-21 17:26:20 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -1,17 +1,37 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
-
+const os = require("os")
+const {runtime} = require(`../lib/functions`)
 cmd({
-    pattern: "alive",
-    desc: "Check bot online or no.",
+    pattern: "system",
+    alias : ["status","botinfo"],
+    desc: "Check up time , ram usage and more",
     category: "main",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
+let status = `
+*🚀ᴄʏʙᴇʀ-ᴍᴅ ꜱʏꜱᴛᴇᴍ🚀*
+┌──────────────────────────────>
+
+> *⏰Uptime:*  ${runtime(process.uptime())}
+> *📟 Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+> *⚙️HostName:* ${os.hostname()}
+> *👨‍💻Owner:* *ᴩʀᴏɢʀᴀᴍᴍᴇʀ ᴀʏᴇꜱʜ*
+
+└──────────────────────────────>
+
+> *🧑‍💻ᴩᴏᴡᴇʀᴅ  ʙʏ  ᴄʏʙᴇʀ-ᴀʏᴇꜱʜ🧑‍💻*
+
+`
+return reply(`${status}`)
+
 }catch(e){
 console.log(e)
 reply(`${e}`)
+
 }
-})
+} )
+
+  
