55364113764c3c86b93a21677299ea7c50535c98
CYBER-MD <167730742+CYBER-MD@users.noreply.github.com>
2024-08-21 18:16:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "Qcgl0aAQ#b0tSLjKSqM6zYLWFV_rvau7M-NjBeMgxweMWN8MceGM",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/87e7ae0d50a3fa8f1ff4e.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG ||"*ʜᴇʏ  ɪ   ᴀᴍ   ᴄʏʙᴇʀ-ᴍᴅ-ʙᴏᴛ👋*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
